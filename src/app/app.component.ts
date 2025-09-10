cef712fd1a3ea7db9a6465acd02b48c7c6fdfabf
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-10 20:45:05 +0500

    10. Styling & Using Our Next Custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,11 @@
 // adding  HeaderComponent to app
 import { Component } from '@angular/core';
 import { HeaderComponent } from './Class_9_to_19/header/header.component';
+import { UserComponent } from './Class_9_to_19/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
