ba86e9e9c74449b9615bfc3f9fd4f8d9ee9394d1
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-10 16:20:32 +0500

    4. Creating a First Custom Component empty coponent

---
+++ src/app/Class_1_to_8/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// decorator
+@Component({
+  selector: 'app-header',
+
+  //======= inline Html =========
+  template: '<h1>Pakistan</h1>',
+})
+export class HeaderComponent {}
