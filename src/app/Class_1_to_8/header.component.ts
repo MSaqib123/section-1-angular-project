206a35266bfef17927f2aa37e2f0acb16a55d897
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-10 17:02:20 +0500

    8. Styling the Header Component & Adding An Image

--- src/app/Class_1_to_8/header.component.ts
+++ src/app/Class_1_to_8/header.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   // template: '<h1>Pakistan</h1>',
   //======= external Html =========
   templateUrl: './header.component.html',
+
+  //======= inline Css =========
+  // styles : ['h1 {color:red}'],
+  //======= external Css =========
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
+//===================================
+// NOte  Class_1_to_8
+//===================================
+//#region
+// adding  HeaderComponent to app
 import { Component } from '@angular/core';
+import { HeaderComponent } from './Class_1_to_8/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {}
+
+//#endregion
