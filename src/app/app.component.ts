862cd126a3531da93d7e992429d570c17fb9aee4
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-10 19:49:01 +0500

    9. Managing & Creating Components with the Angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@
 //===================================
 //#region
 // adding  HeaderComponent to app
+// import { Component } from '@angular/core';
+// import { HeaderComponent } from './Class_1_to_8/header.component';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [HeaderComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css',
+// })
+// export class AppComponent {}
+
+//#endregion
+
+//
+//
+//===================================
+// NOte  Class_9_to_19
+//===================================
+//#region
+// adding  HeaderComponent to app
 import { Component } from '@angular/core';
-import { HeaderComponent } from './Class_1_to_8/header.component';
+import { HeaderComponent } from './Class_9_to_19/header/header.component';
 
 @Component({
   selector: 'app-root',
