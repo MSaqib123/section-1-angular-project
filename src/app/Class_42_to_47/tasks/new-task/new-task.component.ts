07555b7ea8e2bd1668a6913f8ba66df127b8da57
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-11 17:55:25 +0500

    44. Using Directives & Two-Way-Binding

--- src/app/Class_42_to_47/tasks/new-task/new-task.component.ts
+++ src/app/Class_42_to_47/tasks/new-task/new-task.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -10,7 +12,30 @@ export class NewTaskComponent {
   @Input({ required: true }) userId!: string;
   @Output() hideTaskUI = new EventEmitter<void>();
 
+  //==== Without Signals ========
+  inputTitle = '';
+  inputSummery = '';
+  inputDate = '';
+
+  //==== With Signals ========
+  // inputTitle = signal('');
+  // inputSummery = signal('');
+  // inputDate = signal('');
+  // sorry
+
   hideTask() {
+    this.hideTaskUI.emit();
+  }
+
+  onSubmit() {
+    // this. (
+    //   {
+    //     title: this.inputTitle,
+    //     summery: this.inputSummery,
+    //     date: this.inputDate,
+    //   },
+    //   this.userId
+    // );
     this.hideTaskUI.emit();
   }
 }
