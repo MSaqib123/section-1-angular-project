ff23fb23fe5f5f642101ea07bc8b56c42e7d0b57
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-11 16:46:22 +0500

    38. Outputting Task Data in the Task Component

--- src/app/Class_33_to_41/tasks/task/task.component.ts
+++ src/app/Class_33_to_41/tasks/task/task.component.ts
@@ -1,5 +1,13 @@
 import { Component, Input } from '@angular/core';
 
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
+
 @Component({
   selector: 'app-task',
   imports: [],
@@ -7,20 +15,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({ required: true }) userId!: string;
-  @Input({ required: true }) name!: string;
-
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master anguelr',
-      summery: 'asdfklasd fasd kfajsd fkasd fkasdk f asdf jkasdk faksd f adsf',
-      dueDate: '2026-12-31',
-    },
-  ];
-
-  get selectedUserTasks() {
-    return this.tasks.filter((x) => x.userId == this.userId);
-  }
+  @Input({ required: true }) task!: Task;
 }
