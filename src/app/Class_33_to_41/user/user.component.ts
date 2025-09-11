0ce7237bbfbf8d37c4adb287e50ac2f89c3edbf3
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-11 15:34:15 +0500

    33. Outputting List Content

---
+++ src/app/Class_33_to_41/user/user.component.ts
@@ -0,0 +1,62 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// ===================================
+// NOTE Advane Type_Script Type Illisass Way
+// ===================================
+// type User={
+//     id:string,
+//     avatar:string,
+//     name:string;
+// }
+
+// ===================================
+// NOTE Advane Type_Script Type Illisass Way
+// Mostly we used  Interface object
+// ===================================
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  //
+  //
+  //=============== 31 =============
+  //#region
+  //@Input({ required: true }) user!: {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // };
+
+  //=============== 32 =============
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter<string>();
+  get ImagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+  //#endregion
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -83,12 +83,46 @@
 // NOte  Class_31_to_32
 //===================================
 //#region
+// //adding  HeaderComponent to app
+// import { Component } from '@angular/core';
+// import { DUMMY_USERS } from './dummy-users';
+// import { HeaderComponent } from './Class_31_to_32/header/header.component';
+// import { UserComponent } from './Class_31_to_32/user/user.component';
+// import { TasksComponent } from './Class_31_to_32/tasks/tasks.component';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [HeaderComponent, UserComponent, TasksComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css',
+// })
+// export class AppComponent {
+//   users = DUMMY_USERS;
+
+//   selectedUserId!: string;
+//   onSelectUser(id: string) {
+//     this.selectedUserId = id;
+//   }
+
+//   get onSelectUserObj() {
+//     return this.users.find((x) => x.id == this.selectedUserId);
+//   }
+// }
+
+//#endregion
+
+//
+//
+//===================================
+// NOte  Class_33_to_41
+//===================================
+//#region
 //adding  HeaderComponent to app
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from './Class_31_to_32/header/header.component';
-import { UserComponent } from './Class_31_to_32/user/user.component';
-import { TasksComponent } from './Class_31_to_32/tasks/tasks.component';
+import { HeaderComponent } from './Class_33_to_41/header/header.component';
+import { UserComponent } from './Class_33_to_41/user/user.component';
+import { TasksComponent } from './Class_33_to_41/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
