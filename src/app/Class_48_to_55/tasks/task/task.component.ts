ef6e627d920cd08f3ade4c647d5a42bc0f2498af
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-11 18:36:51 +0500

    49. Transforming Template Data with Pipes

--- src/app/Class_48_to_55/tasks/task/task.component.ts
+++ src/app/Class_48_to_55/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../task.model';
-import { CardComponent } from "../../shared/card/card.component";
+import { CardComponent } from '../../shared/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
